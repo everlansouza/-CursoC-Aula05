de8fe110c534f950ee6604867dbae005c7652ee0
Everlan de Souza <everlansoouza@gmail.com>
2021-06-07 20:27:29 -0300

    Aula 05 completa, desenvolvida ao decorrer do curso

---
+++ Convercao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course5 {
+
+    class Convercao {
+    
+        static void Main(string[] args) {
+
+            int num;
+            char caracter;
+            double num2;
+
+            Console.WriteLine("Digite um número inteiro");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite uma unica letra");
+            caracter = char.Parse(Console.ReadLine());
+
+            //dessa forma se colocar "."(ponto) não funcionará de acordo
+            Console.WriteLine("Digite um número componto flutiante");
+            num2 = double.Parse(Console.ReadLine());
+            
+            
+            Console.WriteLine("Digite um número componto flutiante");
+            num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+        }
+    }
+}
